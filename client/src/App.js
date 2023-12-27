3b95a1cc52c49e61ca491fa5923371e6df9cb599
KrillM <mks160615@gmail.com>
2023-12-27 13:42:02 +0900

    내 작업을 위한 사전 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavbarComponents from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <NavbarComponents />
     </div>
   );
 }
